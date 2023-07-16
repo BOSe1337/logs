ba5733134984b11175a19afe9fd138345f3843a7
bobrik <1337@ya.ru>
2023-07-16 18:10:33 +0700

    1

--- api/views.py
+++ api/views.py
@@ -5,8 +5,15 @@ from rest_framework.views import APIView
 
 # Create your views here.
 class UserAccessView(APIView):
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
 
     def post(self, request, *args, **kwargs):
         # return Response(request.META["REMOTE_ADDR"])
-        return Response(request.META['HTTP_X_REAL_IP'])
-    
+        return Response(self.get_client_ip(request))
